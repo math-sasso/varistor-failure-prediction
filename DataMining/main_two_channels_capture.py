bf78f51719dc86aab26a77e53538e92dc8956bf4
Matheus158257 <matheus.sasso17@gmail.com>
2020-07-01 12:16:24 -0300

    alteracoes necessarias pos reuniao

--- DataMining/main_two_channels_capture.py
+++ DataMining/main_two_channels_capture.py
@@ -6,27 +6,23 @@ Last Update on Thu June 30 11:45:00 2020
 @authors: clneto e msasso
 """
 
-import numpy as np
-from struct import unpack
 import matplotlib.pyplot as plt
 import time
-import pandas as pd
 import serial
 import datetime
 from pyvisa.highlevel import ResourceManager
 import sys
-from utils import input_number, input_options, save_exploratory_results, waveform_capture, get_actual_experiment
+import numpy as np
+from struct import unpack
+import pandas as pd
+from utils import input_number, input_options, input_phase
+from utils import save_exploratory_results, waveform_capture, get_actual_experiment
 
-'''
-TODO: Comando para apertar o botão de Run.
-'''
-
+# ----------> Sensor Gates <-------------
 #COM_PORT_TEMPERATURE = 'COM6'
 COM_PORT_TEMPERATURE = ''
-      
 
-def pre_experimemnt_information(dict_results_row,Identificador_varistor=None):
-    
+def pre_experimemnt_information(dict_results_row, Identificador_varistor=None):
     """
     This function returns the updated dict with the pre experiment information
 
@@ -34,84 +30,70 @@ def pre_experimemnt_information(dict_results_row,Identificador_varistor=None):
     :param Identificador_varistor:  String that identifies the varistor. It can be None if it is the first measure on the varistor
     :return: Return the wave Amplitude values and if necessary the time axis value
     """
-    
     # Identificador_varistor
-    if Identificador_varistor == None:
+    if not Identificador_varistor:
         Identificador_varistor = input_number('Qual o número identificador do varistor? ')
     dict_results_row['Identificador_varistor'] = Identificador_varistor
-    
     # Polaridade
-    Polaridade = input_options(question = 'Qual a polaridade? ',options=['p','n'],error_message ='Favor digitar p para positivo ou n para negativo')
+    Polaridade = input_options(question = 'Qual a polaridade? ', options=['p', 'n'], error_message ='Favor digitar p para positivo ou n para negativo')
     dict_results_row['Polaridade'] = Polaridade
-    
     # I_fuga_pre
-    I_fuga_pre = input_number('Qual a corrente de fuga pré experimento em [mA]? ')
+    I_fuga_pre = input_number('Qual a corrente de fuga pré experimento em [uA]? ')
     dict_results_row['I_fuga_pre'] = I_fuga_pre
-    
     # Temp_terminal_pre
     if COM_PORT_TEMPERATURE:
         Temp_terminal_pre = temperature()
     else:
         Temp_terminal_pre = input_number('Qual a temperatura do terminal de fuga pré experimento em [Celcius]? ')
     dict_results_row['Temp_terminal_pre'] = Temp_terminal_pre
-    
     # V_oper
     V_oper = input_number('Qual a tensão pré operação em [V]?')
     dict_results_row['V_oper'] = V_oper
-    
     # V_oper_nominal
     V_oper_nominal = input_number('Qual a tensão de operação nominal em [V]? ')
     dict_results_row['V_oper_nominal'] = V_oper_nominal
-    
     # I_surto_nom
-    I_surto_nom = input_number('Qual a corrente de surto nominal em [mA]? ')
+    I_surto_nom = input_number('Qual a corrente de surto nominal em [uA]? ')
     dict_results_row['I_surto_nom'] = I_surto_nom
-    
-    Fase_nom = input_number('Qual a fase nominal em [graus]? ')
+    # Fase_nom
+    Fase_nom = input_phase('Qual a fase nominal em [graus]? ')
     dict_results_row['Fase_nom'] = Fase_nom
     
-
-    
     return dict_results_row, Identificador_varistor
+    
     
 def pos_experimemnt_information(dict_results_row):
     """
     This function returns the updated dcit with the pos experiment information
-
+    
     :param dict_results_row: dict with pre experiment and experiment name
     :return: Updated dict with the pos experiment information
     """
-    
     # I_fuga_pos
-    I_fuga_pos = input_number('Qual a corrente de fuga pós experimento em [mA]? ')
+    I_fuga_pos = input_number('Qual a corrente de fuga pós experimento em [uA]? ')
     dict_results_row['I_fuga_pos'] = I_fuga_pos
-    
     # Temp_terminal_pos
     Temp_terminal_pos = input_number('Qual a temperatura do terminal de fuga pós experimento em [Celcius]? ')
     dict_results_row['Temp_terminal_pos'] = Temp_terminal_pos
-    
     # Temp_ambiente_pos
     if COM_PORT_TEMPERATURE:
         Temp_ambiente_pos= temperature()
     else:
         Temp_ambiente_pos = input_number('Qual a temperatura ambiente em [Celcius]? ')
     dict_results_row['Temp_ambiente_pos'] = Temp_ambiente_pos
-    
     # V_oper_pos
     V_oper_pos = input_number('Qual a tensão pós operação em [V]? ')
     dict_results_row['V_oper_pos'] = V_oper_pos
     
     return dict_results_row
         
-
-
+        
 def temperature():
     """
     This function returns the temperatura from the serial port
-
+    
     :return: Gets the temperature measured
     """
-    
     ser = serial.Serial(port=COM_PORT_TEMPERATURE)
     temp = ser.read(16)
     temp = str(temp)
@@ -120,33 +102,32 @@ def temperature():
     temp = float(temp)
     return(temp)
 
-
 if __name__ == '__main__':
 
     rm = ResourceManager()
     try:
-        scope = rm.get_instrument(rm.list_resources()[0])#desta maneira não precisamos nos preocupar com o ID do equipamento 
+        scope = rm.get_instrument(rm.list_resources()[0])# desta maneira não precisamos nos preocupar com o ID do equipamento 
     except:
-        print("Verifique se o osciloscópio est-a conectado")
+        print("Verifique se o osciloscópio está conectado")
      
     
-    i,Identificador_varistor  = get_actual_experiment()
+    i, Identificador_varistor  = get_actual_experiment()
     preceed_experiment = True
     encerrar = False
     while(preceed_experiment):
-        #acontece apenas se o usuário digitar q        
+        # acontece apenas se o usuário digitar q        
         proceed_varistor = True
         while(proceed_varistor):
         
             dict_results_row= {}
-            dict_results_row ,Identificador_varistor =  pre_experimemnt_information(dict_results_row) if (i == 1 and not Identificador_varistor) else pre_experimemnt_information(dict_results_row,Identificador_varistor)
+            dict_results_row, Identificador_varistor =  pre_experimemnt_information(dict_results_row) if (i == 1 and not Identificador_varistor) else pre_experimemnt_information(dict_results_row, Identificador_varistor)
            
-            #/* Set up conditional acquisition */
+            # /* Set up conditional acquisition */
             scope.write("ACQUIRE:STATE OFF")
             scope.write("SELECT:CH1 ON")
             scope.write("ACQUIRE:MODE SAMPLE")
             scope.write("ACQUIRE:STOPAFTER SEQUENCE")
-            #/* Acquire waveform data */
+            # /* Acquire waveform data */
             scope.write("ACQUIRE:STATE ON")
             time.sleep(1)
             while scope.query("TRIGGER:STATE?") != 'SAVE\n':
@@ -155,63 +136,33 @@ if __name__ == '__main__':
                 print(scope.query("TRIGGER:STATE?"))# A outra opção é READY
             if scope.query("TRIGGER:STATE?") == 'SAVE\n':
                 time.sleep(1)
-                try:
-                    print('Fluxo normal')
-                    Time,Amplitude = waveform_capture(False,1,scope)
-                    time.sleep(2)
-                    Ensaio = pd.DataFrame({'Time':Time})
-                    Ensaio['Time_Stamp'] = datetime.datetime.now()
-                    Ensaio['V_grampeamento[V]'] = Amplitude  * 250
-                    #fig, ax = plt.subplots()
-                    #ax.plot(Time,Amplitude)
-                    #plt.show()
-                    Amplitude = waveform_capture(True,2,scope)
-                    time.sleep(2)
-                    Ensaio['I_surto_medida[mA]'] = Amplitude * 40
-                    Ensaio['Power[W]'] = Ensaio['V_grampeamento[V]']*Ensaio['I_surto_medida[mA]']
-                    Ensaio['Energy[Joules]'] = Ensaio['Power[W]'] * 6.4e-8
-                    Ensaio.to_csv(f"./Measures/{dict_results_row['Identificador_varistor']}_sample{str(i)}.csv",index=False)
-                    
-                    dict_results_row['Nome_Arquivo_Medidas'] = f"varistor{dict_results_row['Identificador_varistor']}_sample{str(i)}.csv"
-                    #fig, ax = plt.subplots()
-                    #ax.plot(Time,Amplitude)
-                    #plt.show()
-                except:
-                    print('Fluxo sem Time')
-                    Amplitude = waveform_capture(True,1,scope)
-                    time.sleep(2)
-                    Ensaio = pd.DataFrame()
-                    Ensaio['Time_Stamp'] = datetime.datetime.now()
-                    Ensaio['V_grampeamento[V]'] = Amplitude
-                    Amplitude = waveform_capture(True,2,scope)
-                    time.sleep(2)
-                    Ensaio['I_surto_medida[mA]'] = Amplitude* 40
-                    time.sleep(1)
-              
-                    if np.mod(i,2) == 0: # pergunta o porquê dissi
-                        Ensaio['Power[W]'] = Ensaio['V_grampeamento[V]']*Ensaio['I_surto_medida[mA]']
-                        Ensaio['Energy[Joules]'] = Ensaio['Power[W]'] * 6.4e-8
-                        Ensaio.to_csv(f"./Measures/{dict_results_row['Identificador_varistor']}_sample{str(i)}.csv",index=False)
-                    
-                    dict_results_row['Nome_Arquivo_Medidas'] = f"varistor{dict_results_row['Identificador_varistor']}_sample{str(i)}.csv"
-                      
-
+                Time, Amplitude = waveform_capture(1, scope)
+                time.sleep(2)
+                Ensaio = pd.DataFrame({'Time': Time})
+                Ensaio['Time_Stamp'] = datetime.datetime.now()
+                Ensaio['V_grampeamento[V]'] = Amplitude * 250
+                # fig, ax = plt.subplots()
+                # ax.plot(Time,Amplitude)
+                # plt.show()
+                _, Amplitude = waveform_capture(2, scope)
+                time.sleep(2)
+                Ensaio['I_surto_medida[uA]'] = Amplitude * 40
+                Ensaio['Power[W]'] = Ensaio['V_grampeamento[V]']*Ensaio['I_surto_medida[uA]']
+                Ensaio['Energy[Joules]'] = Ensaio['Power[W]'] * 6.4e-8
+                Ensaio.to_csv(f"./Measures/{dict_results_row['Identificador_varistor']}_sample{str(i)}.csv", index=False)
+                dict_results_row['Nome_Arquivo_Medidas'] = f"varistor{dict_results_row['Identificador_varistor']}_sample{str(i)}.csv"
+                # fig, ax = plt.subplots()
+                # ax.plot(Time,Amplitude)
+                # plt.show()
                 print('Dados colhidos com sucesso!')
-            
             dict_results_row = pos_experimemnt_information(dict_results_row) 
             save_exploratory_results(dict_results_row)
-            info = input_options(question ="Favor digitar c para continuar com o mesmo varistor ou n para ir ao próximo varistor ou q para sair: ",options=['c','n','q'])
-            
+            info = input_options(question ="Favor digitar c para continuar com o mesmo varistor ou n para ir ao próximo varistor ou q para sair: ",options=['c', 'n', 'q'])
             if info == 'c':
                 i +=1
             elif info == 'n':
                 i = 1
                 proceed_varistor = False
+                Identificador_varistor = None
             else:
                 sys.exit('Program Finished')
-
-            
-      
-                
-    
-
--- DataMining/utils.py
+++ DataMining/utils.py
@@ -5,13 +5,29 @@ Last Update on Thu June 30 11:45:00 2020
 
 @authors: clneto e msasso
 """
-
 import numpy as np
 from struct import unpack
 import pandas as pd
-import serial
 
+def input_phase(message):
+    """
+    This function manages the phase input as a number if there is a phase or n if dont
 
+    :param message: message text
+    :return: Return the phase information that will be inputed in the dataframe row
+    """
+    infonum = 'info_num'
+    while type(infonum) != float and infonum!= 'n':
+        info = input(message)
+        try:
+            if info!= 'n':
+                infonum = float(info)
+            else:
+                infonum = info
+        except:
+           print("Favor Digitar apenas numeros se houver fase ou n se não houve")      
+    return info
+ 
 
 def input_number(message):
     """
@@ -20,19 +36,17 @@ def input_number(message):
     :param message: message text
     :return: Return the information that will be inputed in the dataframe row
     """
-
     infonum = 'info_num'
     while type(infonum) != float:
         info = input(message)
         try:
             infonum = float(info)
         except:
-           print("Favor Digitar apenas numeros")
-           
-           
+           print("Favor Digitar apenas numeros")        
     return info
-    
-def input_options(question,options, error_message= None):
+
+
+def input_options(question, options, error_message= None):
     """
     This function manages the optional inputs created by the user
 
@@ -40,7 +54,6 @@ def input_options(question,options, error_message= None):
     :param options:  List of options the have to pe choosed by the user as a list of strings
     :return: Return the information that will be inputed in the dataframe row
     """
-
     erm = error_message if error_message else question
     infos_string = 'not_ok'
     while infos_string != 'ok':
@@ -49,9 +62,8 @@ def input_options(question,options, error_message= None):
             infos_string = 'ok'
         else:
             print(erm)
-    
     return info
-      
+
 
 def save_exploratory_results(dict_results_row):
     """
@@ -59,21 +71,18 @@ def save_exploratory_results(dict_results_row):
     
     :param dict_results_row: A list with all values to append in a dataframe
     """
-
     list_results_row = list(dict_results_row.values())
     try:
         exploratory_results = pd.read_csv('./exploratory_results.csv')
     except:
-        exploratory_results = pd.DataFrame(columns=['Identificador_varistor','Polaridade','I_fuga_pre[mA]','Temp_terminal_pre[Celcius]',
-        'V_oper[V]','V_oper_nominal[V]','I_surto_nom[mA]','Fase_nom[graus]','Nome_Arquivo_Medidas','I_fuga_pos[mA]',
-        'Temp_terminal_pos[Celcius]','Temp_ambiente_pos[Celcius]', 'V_oper_pos[V]'])
-        
-    exploratory_results = exploratory_results.append(pd.Series(list_results_row, index=exploratory_results.columns ), ignore_index=True)
-    exploratory_results.to_csv('./exploratory_results.csv',index=False)
+        exploratory_results = pd.DataFrame(columns=['Identificador_varistor', 'Polaridade','I_fuga_pre[uA]', 'Temp_terminal_pre[Celcius]', 
+        'V_oper[V]', 'V_oper_nominal[V]', 'I_surto_nom[uA]', 'Fase_nom[graus]', 'Nome_Arquivo_Medidas', 'I_fuga_pos[uA]',
+        'Temp_terminal_pos[Celcius]', 'Temp_ambiente_pos[Celcius]', 'V_oper_pos[V]'])
+    exploratory_results = exploratory_results.append(pd.Series(list_results_row, index=exploratory_results.columns), ignore_index=True)
+    exploratory_results.to_csv('./exploratory_results.csv', index=False)
 
 
-
-def waveform_capture(time_axis_set,ch,scope):
+def waveform_capture(ch, scope):
     """
     This function capture the wave and return a numpy array representing it
 
@@ -82,42 +91,33 @@ def waveform_capture(time_axis_set,ch,scope):
     :param scope: the scope object
     :return: Return the wave Amplitude values and if necessary the time axis value
     """
-
+    # Data Chanel Captures
     scope.write('DATA:SOU CH'+str(ch))
     scope.write('DATA:WIDTH 1')
     scope.write('DATA:ENC RPB')
-    
-    
+    # Parameters Capturing
     ymult = float(scope.query('WFMPRE:YMULT?'))
     yzero = float(scope.query('WFMPRE:YZERO?'))
     yoff = float(scope.query('WFMPRE:YOFF?'))
     xincr = float(scope.query('WFMPRE:XINCR?'))
-   
-
+    # Data Management
     scope.write('CURVE?')
     data = scope.read_raw()
     headerlen = 2 + int(data[1])
-    header = data[:headerlen]
+    # header = data[:headerlen]
     ADC_wave = data[headerlen:-1]
-    
-    ADC_wave = np.array(unpack('%sB' % len(ADC_wave),ADC_wave))
-    
-    Volts = (ADC_wave - yoff) * ymult  + yzero
-    if time_axis_set == False:
-        Time = np.arange(0, xincr * len(Volts), xincr)
-    
-    if time_axis_set == False:
-        return(Time, Volts)
-    else:
-        return(Volts)
-        
+    ADC_wave = np.array(unpack('%sB' % len(ADC_wave), ADC_wave))
+    Volts = (ADC_wave - yoff) * ymult + yzero
+    Time = np.arange(0, xincr * len(Volts), xincr)
+    return(Time, Volts)
+
+
 def get_actual_experiment():
     """
     This function manages if it will be start from a new varistor or continue measures from where it stop
 
     :return: the measure number and the varistor identifier
     """
-
     info = input("Para começar do zero digite 0, para continuar de onde parou digite 1: ")
     if info == '0':
         i = 1
@@ -132,14 +132,7 @@ def get_actual_experiment():
             for name in unique_file_measeures:
                 if var_name in name:
                     experiments.append(int(name.split(var_name)[1].split('.csv')[0]))
-                
             i = max(experiments)+1 if experiments else 1
         except:
             i = 1
-    
-    return i,Identificador_varistor 
-    
-                  
-                
-    
-
+    return i, Identificador_varistor
